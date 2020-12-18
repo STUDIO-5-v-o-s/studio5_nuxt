52012bc5bddf2f5a32a7780502e47f12e1ca8d77
patrikvadura <patrikvadura@gmail.com>
2020-12-18 17:22:19 +0100

    vytvoření jednotné komponenty pro loga s alternativními "props"

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,25 +1,9 @@
 import { mount } from '@vue/test-utils'
 import Logo from '~/components/brand/Logo.vue'
-import LogoNegative from '~/components/brand/LogoNegative.vue'
-import LogoAlternative from '~/components/brand/LogoAlternative.vue'
-
-describe('LogoNegative', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(LogoNegative)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Logo)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
-
-describe('LogoAlternative', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(LogoAlternative)
     expect(wrapper.vm).toBeTruthy()
   })
 })
