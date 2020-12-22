c9373b65d5b0ac42d1c651e03e80c2285bb83f7b
patrikvadura <patrikvadura@gmail.com>
2020-12-22 21:36:23 +0100

    first stylize Hero for case studie

---
+++ pages/casestudies/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="caseStudies">
+    <HeroCaseStudie />
+
+    <b-container>
+      <b-row>
+        ----test----
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import data from '@/content/cs.json'
+
+export default {
+  components: {
+    HeroCaseStudie: () => import('~/components/hero/HeroCaseStudie')
+  },
+
+  data () {
+    return {
+      data
+    }
+  }
+}
+</script>
+
+<style scoped>
+.caseStudies {
+  padding: 0;
+}
+</style>
