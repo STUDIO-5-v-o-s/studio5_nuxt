eb5fd10c23555395839301c28c055fb3ffa9f50e
patrikvadura <patrikvadura@gmail.com>
2020-12-22 21:08:24 +0100

    přesun do podsložky + test items na úvodní stranu (kvůli testu scroll efektu)

--- pages/index.vue
+++ pages/index.vue
@@ -22,56 +22,12 @@ import data from '@/content/cs.json'
 
 export default {
   components: {
-    Hero: () => import('~/components/HeroVideo')
+    Hero: () => import('~/components/hero/HeroVideo')
   },
 
   data () {
     return {
       data
-    }
-  },
-
-  head () {
-    return {
-      title: this.title,
-      meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.description
-        },
-        {
-          hid: 'og:title',
-          name: 'og:title',
-          content: this.title
-        },
-        {
-          hid: 'og:description',
-          name: 'og:description',
-          content: this.description
-        },
-        {
-          hid: 'og:type',
-          name: 'og:type',
-          content: 'website'
-        },
-        {
-          hid: 'og:image',
-          name: 'og:image',
-          content: this.pageImage
-        },
-        {
-          hid: 'og:url',
-          name: 'og:url',
-          content: `https://www.patrikvadura.cz/${this.id}`
-        },
-        {
-          hide: 'twitter:card',
-          name: 'twitter:card',
-          content: this.pageImage ? 'summary_large_image' : 'summary'
-        }
-      ].filter(x => x.content)
     }
   }
 }
